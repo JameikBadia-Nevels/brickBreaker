06be75a7a70a78117a72ef3216592bacccee2e3a
JameikBadia-Nevels <98413283+JameikBadia-Nevels@users.noreply.github.com>
2022-02-15 20:35:21 -0500

    added my html/javascript/css files

---
+++ brickBreaker.js
@@ -0,0 +1,12 @@
+let brks = document.querySelector("#bricks")
+
+var brickArr = [], numBrickCol = 8, numBrickRow = 4,  
+
+//creating the bricks
+const brickCreate = () => {
+    for(let i = 0; i < numBrickCol; i++){
+        for(let k = 0; k < numBrickRow; k++){
+            // brickArr[brickIndex] = new 
+        }
+    }
+}
